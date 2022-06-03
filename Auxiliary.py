098b400ad4870bbf4259d7e057ce47b2f68f2d2d
Ariam <arivasm90@gmail.com>
2022-06-03 13:31:56 +0200

    Update Auxiliary.py

--- Auxiliary.py
+++ Auxiliary.py
@@ -31,26 +31,6 @@ def load_dataset(name):
     return tf_data, triple
 
 
-def create_model(tf_training, tf_testing, embedding, n_epoch, training_loops, path):
-    results = pipeline(
-        training=tf_training,
-        testing=tf_testing,
-        model=embedding,
-        # Training configuration
-        training_kwargs=dict(
-            num_epochs=n_epoch,
-            use_tqdm_batch=False,
-        ),
-        # Runtime configuration
-        random_seed=1235,
-        device='gpu',
-        training_loop=training_loops,
-    )
-    model = results.model
-    results.save_to_directory(path + embedding)
-    return model, results
-
-
 def filter_prediction(predicted_heads_df, constraint):
     predicted_heads_df = predicted_heads_df[predicted_heads_df.head_label.str.contains(constraint)]
     predicted_heads_df = reset_index(predicted_heads_df)
@@ -71,62 +51,10 @@ def filter_by_type(predicted_heads, triple_data, entity_type):
     return predicted_heads, entity
 
 
-def get_threshold(predicted_heads, percentile):
-    score_values = predicted_heads.score.values
-    threshold = np.percentile(score_values, percentile)
-    threshold_index = predicted_heads.loc[predicted_heads.score > threshold].shape[0]
-    print(threshold, threshold_index)
-    return threshold, threshold_index
-
-
-def get_inflection_point(score_values):
-    # standard deviation
-    stdev = statistics.stdev(score_values)
-    # smooth
-    smooth = gaussian_filter1d(score_values, stdev)
-    # compute second derivative
-    smooth_d2 = np.gradient(np.gradient(smooth))
-    # find switching points
-    infls = np.where(np.diff(np.sign(smooth_d2)))[0]
-    if len(infls) == 1:
-        return infls[0]
-    if len(infls) == 0:
-        return len(score_values)
-    # middle inflection point
-    m_infls = infls[math.ceil(len(infls) / 2)]
-    return m_infls
-
-
-def get_precision(predicted_heads, inflection_index):
-    tp_fp = predicted_heads.iloc[0:inflection_index + 1]
-    tp = tp_fp.loc[tp_fp.in_training == True].shape[0]
-    prec = tp / tp_fp.shape[0]
-    return prec, tp
-
-
-def get_recall(predicted_heads, tp):
-    tp_fn = predicted_heads.loc[predicted_heads.in_training == True].shape[0]
-    rec = tp / tp_fn
-    return rec
-
-
-def get_f_measure(precision, recall):
-    f_measure = 2 * (precision * recall) / (precision + recall)
-    return f_measure
-
-
 def reset_index(predicted_heads):
     predicted_heads.reset_index(inplace=True)
     predicted_heads.drop(columns=['index'], inplace=True)
     return predicted_heads
-
-
-def compute_metrics(predicted_heads, cut_index, model):
-    precision, tp = get_precision(predicted_heads, cut_index)
-    recall = get_recall(predicted_heads, tp)
-    f_measure = get_f_measure(precision, recall)
-    return pd.DataFrame(columns=['precision', 'recall', 'f_measure'],
-                        data=[[precision, recall, f_measure]], index=[model])
 
 
 def plot_score_value(score_values, title):
